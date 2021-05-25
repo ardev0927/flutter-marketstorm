38472ba55119859142e79fce99665a45d8f0a3c6
WangFeng0927 <52046319+WangFeng0927@users.noreply.github.com>
2021-05-25 16:41:21 +0800

    marketstorm_ui

---
+++ lib/routs.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+import 'package:marketstorm/screens/forgot_password/forgot_password.dart';
+import 'package:marketstorm/screens/sign_in/sign_in_screen.dart';
+import 'package:marketstorm/screens/splash/splash_screen.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  SplashScreen.routeName: (context) => SplashScreen(),
+  SignInScreen.routeName: (context) => SignInScreen(),
+  ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
+};
